1c8ded0905a766545aceac2d97a0e45362124a54
kiddo4 <olanrewajutaiwo183@gmail.com>
2024-10-18 10:24:33 -0700

    done with the biometric

--- lib/screens/home_screeen.dart
+++ lib/screens/home_screeen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 class HomeScreeen extends StatefulWidget {
   const HomeScreeen({super.key});
@@ -8,31 +9,77 @@ class HomeScreeen extends StatefulWidget {
 }
 
 class _HomeScreeenState extends State<HomeScreeen> {
+  final LocalAuthentication _auth = LocalAuthentication();
+
+  bool _isAuthenticated = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      floatingActionButton: _authButton(),
       body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Your Balance',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Your Balance',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              '*******',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            )
-          ],
+              _isAuthenticated
+                  ? const Text(
+                      '£200',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  : const Text(
+                      '*******',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+            ],
+          ),
         ),
-      )),
+      ),
+    );
+  }
+
+  Widget _authButton() {
+    return FloatingActionButton(
+      onPressed: () async {
+        if (!_isAuthenticated) {
+          final bool canAuthenticateWithBiometrics =
+              await _auth.canCheckBiometrics;
+          print(canAuthenticateWithBiometrics);
+          if (canAuthenticateWithBiometrics) {
+            try {
+              final bool didAuthenticate = await _auth.authenticate(
+                  localizedReason:
+                      'Authentication needed to show account balance',
+                  options: const AuthenticationOptions(biometricOnly: true));
+              setState(() {
+                _isAuthenticated = didAuthenticate;
+              });
+            } catch (e) {
+              print(e);
+            }
+          }
+        } else {
+          setState(() {
+            _isAuthenticated = false;
+          });
+        }
+      },
+      child: Icon(_isAuthenticated ? Icons.lock : Icons.lock_open),
     );
   }
 }
